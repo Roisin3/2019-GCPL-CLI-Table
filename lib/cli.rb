64c8bf34de98d720dd0c42351917db1497b5c8ae
Roisin3 <38119408+Roisin3@users.noreply.github.com>
2019-10-14 16:15:43 -0500

    Create cli.rb

---
+++ lib/cli.rb
@@ -0,0 +1,24 @@
+def table_printer(array)
+  counter = 0 
+  array.map do |team|
+    counter += 1
+    puts "#{counter}. #{team[0]}"
+  end
+  puts "Please select a team(1 - 18):"
+end
+
+def cli_printer
+  puts "Welcome to 2019 GCPL table printer!"
+  puts "Select a team to view:"
+end
+
+cli_printer
+table_printer(all_team_data_save)
+
+def team_selector
+  input = gets.chomp
+  input = input.to_i
+  Team.all[input]
+end
+
+team_selector
